6ca1c63d854ae01fbeaf98b74173cc3ae55e88f3
alok-upadhyay <alok0412@gmail.com>
2012-09-13 22:44:31 +0530

    first commit

---
+++ tcpnmap.py
@@ -0,0 +1,34 @@
+import socket
+
+HOST = '10.1.6.188'
+PORT = 1
+
+for PORT in range(1, 1025):
+#	print "Checking for port no. : ", PORT
+	socket.setdefaulttimeout(0.5)
+	mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		mysocket.connect((HOST, PORT), )
+	except socket.error, msg:
+		#print msg
+		mysocket.close()
+		mysocket = None
+		try:
+			print PORT, socket.getservbyport(PORT), "down"
+		except socket.error, msg:
+			print "UNKNOWN PORT", "down"
+		continue
+	except socket.timeout, msg:
+		mysocket.close()
+                mysocket = None
+                try:
+                        print PORT, socket.getservbyport(PORT), "down"
+                except socket.error, msg:
+                        print "UNKNOWN PORT", "down"
+
+		continue
+	
+	print PORT, socket.getservbyport(PORT), "up"
+	mysocket.close()
+	mysocket = None
+
